a95dbec0cff3ebff1c5caff0471917b715d381c6
Paradada <157872748+Paradada@users.noreply.github.com>
2026-09-15 10:05:35 +0800

    20260915 channels 128->96

--- model.py
+++ model.py
@@ -17,6 +17,7 @@ class AttentionDTI(nn.Module):
         super(AttentionDTI, self).__init__()
         self.dim = hp.char_dim
         self.conv = hp.conv
+        self.conv_channels = [32, 64, 96]
         self.drug_MAX_LENGH = drug_MAX_LENGH
         self.drug_kernel = hp.drug_kernel
         self.protein_MAX_LENGH = protein_MAX_LENGH
@@ -25,26 +26,26 @@ class AttentionDTI(nn.Module):
         self.protein_embed = nn.Embedding(26, self.dim,padding_idx=0)
         self.drug_embed = nn.Embedding(65, self.dim,padding_idx=0)
         self.Drug_CNNs = nn.Sequential(
-            nn.Conv1d(in_channels=self.dim, out_channels= self.conv,  kernel_size=self.drug_kernel[0]),
+            nn.Conv1d(in_channels=self.dim, out_channels=self.conv_channels[0], kernel_size=self.drug_kernel[0]),
             nn.ReLU(),
-            nn.Conv1d(in_channels=self.conv, out_channels= self.conv*2,  kernel_size=self.drug_kernel[1]),
+            nn.Conv1d(in_channels=self.conv_channels[0], out_channels=self.conv_channels[1], kernel_size=self.drug_kernel[1]),
             nn.ReLU(),
-            nn.Conv1d(in_channels=self.conv*2, out_channels= self.conv*4,  kernel_size=self.drug_kernel[2]),
+            nn.Conv1d(in_channels=self.conv_channels[1], out_channels=self.conv_channels[2], kernel_size=self.drug_kernel[2]),
             nn.ReLU(),
         )
         self.Drug_max_pool = nn.MaxPool1d(self.drug_MAX_LENGH-self.drug_kernel[0]-self.drug_kernel[1]-self.drug_kernel[2]+3)
         self.Protein_CNNs = nn.Sequential(
-            nn.Conv1d(in_channels=self.dim, out_channels=self.conv, kernel_size=self.protein_kernel[0]),
+            nn.Conv1d(in_channels=self.dim, out_channels=self.conv_channels[0], kernel_size=self.protein_kernel[0]),
             nn.ReLU(),
-            nn.Conv1d(in_channels=self.conv, out_channels=self.conv * 2, kernel_size=self.protein_kernel[1]),
+            nn.Conv1d(in_channels=self.conv_channels[0], out_channels=self.conv_channels[1], kernel_size=self.protein_kernel[1]),
             nn.ReLU(),
-            nn.Conv1d(in_channels=self.conv * 2, out_channels=self.conv * 4, kernel_size=self.protein_kernel[2]),
+            nn.Conv1d(in_channels=self.conv_channels[1], out_channels=self.conv_channels[2], kernel_size=self.protein_kernel[2]),
             nn.ReLU(),
         )
         self.Protein_max_pool = nn.MaxPool1d(self.protein_MAX_LENGH - self.protein_kernel[0] - self.protein_kernel[1] - self.protein_kernel[2] + 3)
-        self.attention_layer = nn.Linear(self.conv*4,self.conv*4)
-        self.protein_attention_layer = nn.Linear(self.conv * 4, self.conv * 4)
-        self.drug_attention_layer = nn.Linear(self.conv * 4, self.conv * 4)
+        self.attention_layer = nn.Linear(self.conv_channels[2], self.conv_channels[2])
+        self.protein_attention_layer = nn.Linear(self.conv_channels[2], self.conv_channels[2])
+        self.drug_attention_layer = nn.Linear(self.conv_channels[2], self.conv_channels[2])
         self.dropout1 = nn.Dropout(0.1)
         self.dropout2 = nn.Dropout(0.1)
         self.dropout3 = nn.Dropout(0.1)
@@ -52,7 +53,7 @@ class AttentionDTI(nn.Module):
         self.tanh = nn.Tanh()
         self.sigmoid = nn.Sigmoid()
         self.leaky_relu = nn.LeakyReLU()
-        self.fc1 = nn.Linear(self.conv*8, 1024)
+        self.fc1 = nn.Linear(self.conv_channels[2] * 2, 1024)
         self.fc2 = nn.Linear(1024, 1024)
         self.fc3 = nn.Linear(1024, 512)
         self.out = nn.Linear(512, 2)
